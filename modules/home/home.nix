5bb5a9b529fb5983cf4ece8dfcca8f87ce952f1b
guuhmartins <245439479+guuhmartins@users.noreply.github.com>
2026-04-21 22:50:41 -0400

    feat(home): finalize home-manager configuration

---
+++ modules/home/home.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ./shell/kitty.nix
+    ./dev-tools/vscode.nix
+    ./dev-tools/languages.nix
+    ./apps/default.nix
+    ./tui/neovim/default.nix
+  ];
+  home.username = "guuh";
+  home.homeDirectory = "/home/guuh";
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
+}

e03b219abc98ba56e9021f31bff475a8d10eb012
guuhmartins <245439479+guuhmartins@users.noreply.github.com>
2026-04-21 22:41:42 -0400

    feat: initial flake setup

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Guuh's NixOS & Home Manager Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Snow-Niri / iNiR references
+    inir = {
+      url = "github:snowarch/iNiR";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./modules/system/configuration.nix
+        ];
+      };
+    };
+}

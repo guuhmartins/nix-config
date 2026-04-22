9db1a137d798c7e2bba7f030db4ab00c1cb34a40
guuhmartins <245439479+guuhmartins@users.noreply.github.com>
2026-04-21 22:50:18 -0400

    perf(system): use latest linux kernel for ryzen support

--- modules/system/hardware.nix
+++ modules/system/hardware.nix
@@ -3,4 +3,5 @@
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }

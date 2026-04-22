d090c26a8e5d810bd2f75a5f78bd884eda2717b2
guuhmartins <245439479+guuhmartins@users.noreply.github.com>
2026-04-21 22:50:41 -0400

    feat(system): finalize main configuration

---
+++ modules/system/configuration.nix
@@ -0,0 +1,17 @@
+{ pkgs, inputs, ... }:
+{
+  imports = [
+    ./hardware.nix
+    ./gnome.nix
+    ./games.nix
+  ];
+  networking.hostName = "nixos";
+  time.timeZone = "America/Sao_Paulo";
+  i18n.defaultLocale = "pt_BR.UTF-8";
+  users.users.guuh = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "24.11";
+}

ae61e5d4a44ddc88bbc6bc6cea8d44dbf04f757f
guuhmartins <245439479+guuhmartins@users.noreply.github.com>
2026-04-21 22:50:23 -0400

    feat(desktop): add tiling and dock extensions

--- modules/system/gnome.nix
+++ modules/system/gnome.nix
@@ -2,8 +2,16 @@
 {
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
     desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
   };
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.forge
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.blur-my-shell
+    gnome-tweaks
+  ];
 }
